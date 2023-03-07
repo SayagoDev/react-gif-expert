39159d423b358ebff10f7d4fa96bafe001b872d4
Saúl Sáyago <sayagoadd@gmail.com>
2023-03-07 06:59:36 -0600

    test: pruebas en el componente <AddCategory />

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
     const [inputValue, setInputValue] = useState('');
@@ -13,12 +14,12 @@ export const AddCategory = ({ onNewCategory }) => {
         if (newInputValue.length <= 1) return;
 
         /* setCategories(categories => [inputValue, ...categories]); */
+        setInputValue('');
         onNewCategory( newInputValue );
-        setInputValue("");
     };
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} aria-label="form">
             <input
                 type='text'
                 placeholder='Buscar gifs'
@@ -28,3 +29,7 @@ export const AddCategory = ({ onNewCategory }) => {
         </form>
     );
 };
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
+}
